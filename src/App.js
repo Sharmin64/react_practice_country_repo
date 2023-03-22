e8cacbce491f474aaed3a66c8e0b60a18038836a
Sharmin64 <lizaeatsbanana@gmail.com>
2023-03-22 23:11:12 +0600

    folder added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import Countries from "./components/Countries/Countries";
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
+      <Countries></Countries>
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
+//function LoadCountries() {
+//  const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch("https://restcountries.com/v3.1/all")
-      .then((res) => res.json())
-      .then((data) => setCountries(data));
-  }, []);
-  return (
-    <div>
-      <h1>Roam around the world With me !!!!</h1>
-      <h3>Available countries: {countries.length}</h3>
-      {countries.map((country) => (
-        <Country
-          name={country.name.common}
-          population={country.population}
-        ></Country>
-      ))}
-    </div>
-  );
-}
+//  useEffect(() => {
+//    fetch("https://restcountries.com/v3.1/all")
+//      .then((res) => res.json())
+//      .then((data) => setCountries(data));
+//  }, []);
+//  return (
+//    <div>
+//      <h1>Roam around the world With me !!!!</h1>
+//      <h3>Available countries: {countries.length}</h3>
+//      {countries.map((country) => (
+//        <Country
+//          name={country.name.common}
+//          population={country.population}
+//        ></Country>
+//      ))}
+//    </div>
+//  );
+//}
 
-function Country(props) {
-  return (
-    <div>
-      <h2>Name:{props.name}</h2>
-      <h4>Population:{props.population}</h4>
-    </div>
-  );
-}
+//function Country(props) {
+//  return (
+//    <div>
+//      <h2>Name:{props.name}</h2>
+//      <h4>Population:{props.population}</h4>
+//    </div>
+//  );
+//}
 export default App;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Country.css";
+
+const Country = (props) => {
+  const { name, population, area, region } = props.country;
+  return (
+    <div className="country">
+      <h2>Country Name:{name.common} </h2>
+      <p>Population: {population}</p>
+      <p>
+        <small>Area: {area}</small>
+      </p>
+      <p>
+        <small>Continent: {region}</small>
+      </p>
+    </div>
+  );
+};
+
+export default Country;
